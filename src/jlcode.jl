3aa6c30bc47950ea55e4a1b62682d559426fa458
Rratic <Rratic@163.com>
2022-05-15 21:56:38 +0800

    complete:jlcode

--- src/jlcode.jl
+++ src/jlcode.jl
@@ -12,18 +12,29 @@ function col(content::String,co::String;br=true)
 	t=replace(t,">"=>"&gt;")
 	return "<span class=\"$co\">$(br ? replace(t,"\n"=>"<br />") : t)</span>"
 end
+#= todo:
+* 长转义
+* 末尾处理
+* 正则表达式
+=#
 function jlcode(co::String)
 	co=replace(co,"\r"=>"")
 	repl=false
 	s=""
-	stack=Vector{String}() # " """ ` $(
+	stack=Vector{UInt8}()
+	#=
+	0	$(
+	1	"
+	2	`
+	3	"""
+	=#
 	len=length(co)
 	pre=1
 	i=1
 	emp=false
 	dealf=(to::Int=i-1)->begin
 		s*=col(co[pre:to],emp ? "plain" :
-			last(stack)=="\$(" ? "insert" : "string"
+			last(stack)==0x0 ? "insert" : "string"
 		)
 	end
 	try
@@ -31,7 +42,7 @@ function jlcode(co::String)
 	while i<=len
 		ch=co[i]
 		emp=isempty(stack)
-		emp2=emp || last(stack)=="\$("
+		emp2=emp || last(stack)==0x0
 		if emp && (ch=='\n' || i==1) # REPL特殊处理尝试
 			if ch=='\n' i+=1 end
 			if findnext("julia> ",co,i)!==nothing
@@ -57,7 +68,7 @@ function jlcode(co::String)
 					s*=col(co[f],"repl-pkg")
 					i=f.stop+1
 					pre=i
-				elseif repl && findnext("ERROR:",co,i) # "caused by:"
+				elseif repl && findnext("ERROR:",co,i)!==nothing # "caused by:"
 					dealf()
 					s*=col("ERROR:","repl-error")
 					i+=6
@@ -95,20 +106,18 @@ function jlcode(co::String)
 			end
 			i=j
 			pre=j
-		elseif 'A'<=ch<='Z' # 推测是类型
+		elseif emp2 && 'A'<=ch<='Z' # 推测是类型
 			dealf()
 			j=i+1
 			st=0
 			while j<=len
-				if Base.is_id_char(co[j])
-					j+=1
-				elseif co[j]=='{' st+=1
+				if co[j]=='{' st+=1
 				elseif co[j]=='}'
-					if st==0 break
-					else st-=1 end
-				else
+					st==0 ? break : st-=1
+				elseif !Base.is_id_char(co[j])
 					break
 				end
+				j+=1
 			end
 			if j>len
 				s*=col(co[i:len],"type")
@@ -119,23 +128,70 @@ function jlcode(co::String)
 				pre=j
 			end
 		elseif ch=='\"'
-		elseif ch=='\''
-		elseif ch=='\\'
-			if emp2
-				i+=1
-			else
-				# todo : \u ...
+			la=last(stack)
+			if emp || la==0x0 # 新字符串
 				dealf()
-				s*=col(co[i:i+1],"escape")
-				i+=2
-				pre=i
+				if co[i+1]=='"'
+					if i+1==len # 末尾&空
+						s*=col("\"\"","string")
+						break
+					elseif co[i+2]!='"' # 空字符串
+						s*=col("\"\"","string")
+						i+=2
+						continue
+					end
+					# 多行字符串
+					push!(stack,0x3)
+					i+=3
+				else
+					push!(stack,0x1)
+					i+=1
+				end
+			elseif la==0x1
+				if i==len # 末尾
+					s*=col(co[pre:len],"string")
+					break
+				else
+					s*=col(co[pre:i],"string")
+					i+=1
+					pre=i
+					pop!(stack)
+				end
+			elseif la==0x3
+				if i>len-2 break end
+				if co[i+1]=='"' && co[i+2]=='"'
+					s*=col(co[pre:i+2],"string")
+					i+=3
+					pre=i
+					pop!(stack)
+				else
+					i+=1
+				end
+			elseif la==0x2
+				i+=1
 			end
+		elseif emp2 && ch=='\''
+			dealf()
+			if j>len-2 break end
+			if ch[i+1]=='\\'
+				s*=col(co[i:i+3],"string")
+				i+=4
+			else
+				s*=col(co[i:i+2],"string")
+				i+=3
+			end
+			pre=i
+		elseif !emp2 && ch=='\\'
+			dealf()
+			s*=col(co[i:i+1],"escape")
+			i+=2
+			pre=i
 		elseif ch=='$'
 			j=i+1
 			if ch[j]=='('
 				dealf()
 				pre=i
-				push!(stack,"\$(")
+				push!(stack,0x0)
 				i+=2
 			elseif Base.is_id_start_char(ch[j])
 				j+=1
@@ -157,15 +213,44 @@ function jlcode(co::String)
 			else
 				i+=1
 			end
-		elseif ch=='@'
+		elseif emp2 && ch=='@'
 			j=i+1
-		elseif ch=='`'
-		elseif '0'<=ch<='9' # 推测是数字
+			if Base.is_id_start_char(ch[j])
+				j+=1
+				while j<=len
+					if Base.is_id_char(ch[j])
+						j+=1
+					else
+						break
+					end
+				end
+				if j>len
+					s*=col(co[i:len],"macro")
+					break
+				else
+					s*=col(co[i:j-1],"macro")
+					i=j
+					pre=j
+				end
+			else
+				i+=1
+			end
+		elseif emp2 && ch=='`'
+			if emp || last(stack)!=0x2
+				dealf()
+				push!(stack,0x2)
+			else
+				s*=col(co[pre:i],"string")
+				pop!(stack)
+			end
+			i+=1
+			pre=i
+		elseif emp2 && '0'<=ch<='9' # 推测是数字
 			dealf()
 			j=i+1
 			if j!=len && (co[j]=='x' || co[j]=='o') j+=1 end
 			while j<=len
-				if 0<=co[j]<=9 || 'a'<=co[j]<='f' || co[j]=='_'
+				if '0'<=co[j]<='9' || 'a'<=co[j]<='f' || co[j]=='_'
 					j+=1
 				else
 					break
@@ -179,7 +264,7 @@ function jlcode(co::String)
 				i=j
 				pre=j
 			end
-		elseif ch=='#'
+		elseif emp2 && ch=='#'
 			dealf()
 			if i==len
 				s*=col("#","comment")
@@ -203,10 +288,11 @@ function jlcode(co::String)
 				end
 			end
 			pre=i
-		elseif ch==')' && (!emp) && last(stack)=="\$)"
+		elseif !emp2 && ch==')'
 			s*=col(co[pre:i],"insert")
 			i+=1
 			pre=i
+			pop!(stack)
 		else
 			i+=1
 		end
